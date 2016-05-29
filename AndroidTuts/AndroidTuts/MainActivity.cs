0b470beb05c9c9ded059abf31f36eef9f8b1ade3
frets.frits <frats.frets.frits@gmail.com>
2016-05-29 23:05:38 +0800

    blah blah

--- AndroidTuts/AndroidTuts/MainActivity.cs
+++ AndroidTuts/AndroidTuts/MainActivity.cs
@@ -8,23 +8,17 @@ using Android.OS;
 
 namespace AndroidTuts
 {
-    [Activity(Label = "AndroidTuts", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            ActionBar.Hide();
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
--- AndroidTuts/AndroidTuts/Resources/Resource.Designer.cs
+++ AndroidTuts/AndroidTuts/Resources/Resource.Designer.cs
@@ -60,11 +60,14 @@ namespace AndroidTuts
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
-			
 			// aapt resource value: 0x7f050001
-			public const int textClock1 = 2131034113;
+			public const int btnSignIn = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int btnSignUp = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int txtCreateAccount = 2131034112;
 			
 			static Id()
 			{
@@ -95,11 +98,8 @@ namespace AndroidTuts
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int ApplicationName = 2130968576;
 			
 			static String()
 			{
