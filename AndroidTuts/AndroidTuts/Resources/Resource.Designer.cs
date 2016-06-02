65309f8c63939316c371d182ce5ef1cd891fe866
Johnfrits <Johnfrits@JOHNFRITS>
2016-06-02 19:44:51 +0800

    background color changes

--- AndroidTuts/AndroidTuts/Resources/Resource.Designer.cs
+++ AndroidTuts/AndroidTuts/Resources/Resource.Designer.cs
@@ -48,7 +48,13 @@ namespace AndroidTuts
 			public const int buttonSignInStyle = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int buttonSignUpStyle = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int progressbarStyle = 2130837507;
 			
 			static Drawable()
 			{
